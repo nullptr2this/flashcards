96de31905e019c475707db9cf6c5468e0ca5caff
Jon Cobb <jon.cobber@gmail.com>
2021-01-21 02:20:59 -0500

    adds simple correct/incorrect answer counters

--- app/src/main/java/io/github/nullifythis/android/flashcards/MainActivity.kt
+++ app/src/main/java/io/github/nullifythis/android/flashcards/MainActivity.kt
@@ -13,12 +13,16 @@ class MainActivity : AppCompatActivity() {
         private const val KEY_REMAINING_WORDS = "KEY_REMAINING_WORDS"
         private const val KEY_INCORRECT_WORDS = "KEY_INCORRECT_WORDS"
         private const val KEY_IS_SHOWING_RETRIES = "KEY_IS_SHOWING_RETRIES"
+        private const val KEY_CORRECT_ANSWER_COUNT = "KEY_CORRECT_ANSWERS"
+        private const val KEY_INCORRECT_ANSWER_COUNT = "KEY_INCORRECT_ANSWERS"
     }
 
     private var remainingWords: MutableList<String> = mutableListOf()
     private var incorrectWords: MutableList<String> = mutableListOf()
 
     private var isShowingRetries = false
+    private var correctAnswerCount = 0
+    private var incorrectAnswerCount = 0
 
     private val hasWordsRemaining: Boolean
         get() = remainingWords.isNotEmpty()
@@ -48,12 +52,16 @@ class MainActivity : AppCompatActivity() {
         outState.putStringArray(KEY_REMAINING_WORDS, remainingWords.toTypedArray())
         outState.putStringArray(KEY_INCORRECT_WORDS, incorrectWords.toTypedArray())
         outState.putBoolean(KEY_IS_SHOWING_RETRIES, isShowingRetries)
+        outState.putInt(KEY_CORRECT_ANSWER_COUNT, correctAnswerCount)
+        outState.putInt(KEY_INCORRECT_ANSWER_COUNT, incorrectAnswerCount)
     }
 
     private fun initializeWordList() {
         remainingWords = Words.createRandomizedList().toMutableList()
         incorrectWords = mutableListOf()
         isShowingRetries = false
+        correctAnswerCount = 0
+        incorrectAnswerCount = 0
     }
 
     private fun restoreWordList(savedState: Bundle) {
@@ -64,6 +72,8 @@ class MainActivity : AppCompatActivity() {
             savedState.getStringArray(KEY_INCORRECT_WORDS)
         ).toMutableList()
         isShowingRetries = savedState.getBoolean(KEY_IS_SHOWING_RETRIES)
+        correctAnswerCount = savedState.getInt(KEY_CORRECT_ANSWER_COUNT, 0)
+        incorrectAnswerCount = savedState.getInt(KEY_INCORRECT_ANSWER_COUNT, 0)
     }
 
     private fun requestResetWordList() {
@@ -75,6 +85,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun onWordAnsweredCorrectly() {
         if (remainingWords.isEmpty()) { return }
+        correctAnswerCount++
         remainingWords.removeFirst()
         continueWithRetriesIfRequired()
         updateDisplay()
@@ -82,6 +93,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun onWordAnsweredIncorrectly() {
         if (remainingWords.isEmpty()) { return }
+        incorrectAnswerCount++
         incorrectWords.add(remainingWords.removeFirst())
         continueWithRetriesIfRequired()
         updateDisplay()
@@ -108,5 +120,7 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+        text_word_count_correct.text = String.format("%03d", correctAnswerCount)
+        text_word_count_incorrect.text = String.format("%03d", incorrectAnswerCount)
     }
 }
